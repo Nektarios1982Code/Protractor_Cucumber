117ca93cf9f2d8979f4dc517fd873ac4d5b8dab7
nektarios <s_aris@hotmail.com>
2020-02-27 00:08:26 +0200

    Add cucumber requirements in protractor-conf.js file

--- protractor-conf.js
+++ protractor-conf.js
@@ -1,10 +1,17 @@
 exports.config = {
     //directConnect: true,
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    framework: 'jasmine',
-    //frameworkPath: require.resolve('protractor-cucumber-framework'),
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
     capabilities: {
         'browserName': 'chrome'
     },
-    specs: ['protractor-specs.js']
+    specs: ['./features/*.feature'],
+    cucumberOpts: {
+        tags: false,
+        // require step definitions
+        require: [
+            './features/stepDefinitions/*.js', // accepts a glob
+        ]
+    }
 };
